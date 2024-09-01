33c50b27e207d45eff2d34af813f603c390b55f1
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-09-01 14:02:03 +0700

    add is_published and can_vote functions

--- polls/models.py
+++ polls/models.py
@@ -7,12 +7,24 @@ from django.utils import timezone
 class Question(models.Model):
     """A question object with a question_text and a publication date."""
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", default=timezone.now)
+    end_date = models.DateTimeField("end date", null=True, blank=True)
 
     def was_published_recently(self):
         """Returns True if the question was published within the last day."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """Check if the question is published. Returns True if current date is on or after publication date."""
+        return timezone.now() >= self.pub_date
+
+    def can_vote(self):
+        """Check if voting is allowed for this question.Returns True if current date is between pub_date and end_date"""
+        now = timezone.now()
+        if self.end_date:
+            return self.pub_date <= now <= self.end_date
+        return self.pub_date <= now
 
     def __str__(self):
         """Returns the question_text of the question."""
