2f5b4aa5603d82df51a9130ab9e20408af66269a
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-09-01 19:07:44 +0700

    verify docstrings and pep8

--- polls/migrations/0003_vote.py
+++ polls/migrations/0003_vote.py
@@ -16,10 +16,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Vote',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True,
+                                           serialize=False,
+                                           verbose_name='ID')),
                 ('voted_at', models.DateTimeField(auto_now_add=True)),
-                ('choice', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.choice')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('choice', models.ForeignKey(on_delete=django.db.
+                                             models.deletion.CASCADE,
+                                             to='polls.choice')),
+                ('user', models.ForeignKey(on_delete=django.db.
+                                           models.deletion.CASCADE,
+                                           to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'unique_together': {('user', 'choice')},
--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from datetime import timedelta
 from .models import Question
 
+
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
@@ -14,6 +15,7 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
@@ -22,14 +24,18 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
-        self.assertListEqual(list(response.context["latest_question_list"]), [])
+        self.assertListEqual(list(
+            response.context["latest_question_list"]),
+            [])
 
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future
         returns a 302 not found.
         """
-        future_question = create_question(question_text="Future question.", days=5)
+        future_question = create_question(
+            question_text="Future question.",
+            days=5)
         url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
@@ -39,7 +45,9 @@ class QuestionIndexViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text="Past Question.", days=-5)
+        past_question = create_question(
+            question_text="Past Question.",
+            days=-5)
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
@@ -69,10 +77,12 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1]
         )
 
+
 class QuestionModelTests(TestCase):
     def test_is_published_future_date(self):
         """Questions with a future pub_date are not published."""
-        future_question = Question(pub_date=timezone.now() + timedelta(days=30))
+        future_question = Question(
+            pub_date=timezone.now() + timedelta(days=30))
         self.assertFalse(future_question.is_published())
 
     def test_is_published_default_date(self):
@@ -87,7 +97,8 @@ class QuestionModelTests(TestCase):
 
     def test_can_vote_before_pub_date(self):
         """Cannot vote if the current date is before the pub_date."""
-        future_question = Question(pub_date=timezone.now() + timedelta(days=30))
+        future_question = Question(
+            pub_date=timezone.now() + timedelta(days=30))
         self.assertFalse(future_question.can_vote())
 
     def test_can_vote_between_pub_and_end_date(self):
